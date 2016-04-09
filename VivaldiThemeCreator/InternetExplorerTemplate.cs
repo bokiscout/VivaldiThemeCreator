eb1c511e2190d724b8250dadb0c3245565adccaa
Borche Jankovski <bokiscout@hotmail.com>
2016-04-09 05:50:52 +0200

    Updated for Vivaldi 1.0.435.42

--- VivaldiThemeCreator/InternetExplorerTemplate.cs
+++ VivaldiThemeCreator/InternetExplorerTemplate.cs
@@ -14,20 +14,27 @@ namespace VivaldiThemeCreator
         // default constructor set the template
         public InternetExplorerTemplate(){
             template =
-@"/* everything in one row is not usable on small screens/windows */
-@media screen and (max-width: 950px) {
+                @"/* everything in one row is not usable on small screens/windows */
+@media screen and (max-width: 900px) {
   #tabs-container{
     width: 100% !important;
     margin-left: 0px !important;
   }
 
+/*  #tabs-container .resize{
+    left: 40px !important;
+  }*/
+
+  #main{
+    margin-top: 0px !important;
+  }
+
   #main.left > .toolbar{
     width: 100% !important;
-    position: static !important;
-    top: 50px !important;
-    left: 34px !important;
+    /*left: 34px !important;*/
     /*background-color: rgba(255, 0, 0, 0) !important;*/
   }
+
 }
 
 /*
@@ -37,7 +44,6 @@ namespace VivaldiThemeCreator
 ///                   ///
 /////////////////////////
 */
-
 #header{
   /*background-color: gray;*/
 }
@@ -50,24 +56,26 @@ namespace VivaldiThemeCreator
 ///                   ///
 /////////////////////////
 */
+#main{
+    margin-top: -34px;
+    z-index: 10;
+}
 
 #main.left > .toolbar{
-  width: 50%;
+  width: 40%;
   padding-right: -100px;
-  position: absolute;
-  top: -34px;
-  left: 34px;
   background-color: rgba(255, 0, 0, 0);
 }
 
 #tabs-container{
-  width: 50%;
+  width: 60%;
   float: right;
 }
 
 /*move tabs right some pixeles to prevent overlap with adressbar*/
 #tabs-container > .resize{
   margin-left: 135px;
+  z-index: 12;
 }
 
 
@@ -81,7 +89,25 @@ namespace VivaldiThemeCreator
 .searchfield{
   display: none !important;
 }
-";
+
+
+/*
+/////////////////////////
+///                   ///
+///   vivaldi button  ///
+///                   ///
+/////////////////////////
+*/
+.vivaldi{
+  display: none;
+}
+
+.window-buttongroup{
+  z-index: 99999;
+
+/* end */
+
+}";
         } // default constructor ends here
 
         public String GetTemplate()
